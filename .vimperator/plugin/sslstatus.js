33d28176cc45e853b5d698bef0f8fe2f8331c354
Puxirepublic <huj1130.extraction@gmail.com>
2014-07-08 18:15:18 +0800

    update.

---
+++ .vimperator/plugin/sslstatus.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm", modules);
+
+const progressListener = {
+    QueryInterface: XPCOMUtils.generateQI([Ci.nsISupportsWeakReference, Ci.nsIWebProgressListener]),
+
+    onSecurityChange: function onSecurityChange(webProgress, request, state) {
+        if (state & Ci.nsIWebProgressListener.STATE_IS_INSECURE)
+            highlight.set("StatusLine", "color:#D2D0D5;font-family:lemon;font-size:10px;");
+        else if (state & Ci.nsIWebProgressListener.STATE_IS_BROKEN)
+            highlight.set("StatusLine", "color:#AD96EB; background:#171717;font-family:lemon;font-size:10px;");
+        else if (state & Ci.nsIWebProgressListener.STATE_IDENTITY_EV_TOPLEVEL)
+            highlight.set("StatusLine", "color:#fea63c; background:#171717;font-family:lemon;font-size:10px;");
+        else if (state & Ci.nsIWebProgressListener.STATE_SECURE_HIGH)
+            highlight.set("StatusLine", "color:#00aa88; background:#171717;font-family:lemon;font-size:10px;");
+    }
+};
+
+config.browser.addProgressListener(progressListener, Ci.nsIWebProgress.NOTIFY_SECURITY);
