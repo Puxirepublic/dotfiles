ab5c886250390474397da2b9e1c1846060cd1cb5
Puxirepublic <huj1130.extraction@gmail.com>
2014-09-04 15:43:42 +0800

    my homepage for ff

--- home/media/date.js
+++ home/media/date.js
@@ -6,12 +6,22 @@ function Time() {
 	var yyyy=today.getFullYear();
 	var mo=today.getMonth()+1;
 	var dd=today.getDate();
+        var d = new Date();
+        var weekday = new Array(7);
+        weekday[0]=  "天";
+        weekday[1] = "一";
+        weekday[2] = "二";
+        weekday[3] = "三";
+        weekday[4] = "四";
+        weekday[5] = "五";
+        weekday[6] = "六";
+        var n = weekday[d.getDay()]; 
 	hh = check(hh);
 	mm = check(mm);
 	ss = check(ss);
 	mo = check(mo);
 	dd = check(dd);
-	document.getElementById('date').innerHTML = hh+""+mm;
+	document.getElementById('date').innerHTML = n+"";
 	var t = setTimeout(function(){Time()},500);
 }
 function check(i) {
